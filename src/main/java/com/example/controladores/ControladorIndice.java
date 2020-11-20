c25ac22e3747e2d708a5c7d038c4751ff40b5beb
Fernando <Fernando@pc1610>
2020-11-20 11:19:17 +0100

    Casi index terminado

--- src/main/java/com/example/controladores/ControladorIndice.java
+++ src/main/java/com/example/controladores/ControladorIndice.java
@@ -1,14 +1,38 @@
 package com.example.controladores;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.example.entidades.Producto;
+import com.example.servicios.ProductoServicio;
 
+@Controller
 public class ControladorIndice {
+	
+	/*
+	@Autowired
+	ProductoServicio productoService;
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/index")
+	public ModelAndView listarOcho() {
+		ModelAndView mav = new ModelAndView();
+		List<Producto> lProducto = productoService.listarOcho();
+		mav.addObject("ProductosOcho", lProducto);
+		mav.setViewName("/index");
+		return mav;
+	}*/
+	
+
 	@GetMapping({"/","/index"})
 	public String index(Model modelo) {
 
-		return "index";
+		return "/index";
 	}
 }
