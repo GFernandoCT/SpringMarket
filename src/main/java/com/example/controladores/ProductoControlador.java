6ead1209dc267ee1bbd05d336cefe2eb45369b72
Alexander <Alexander@pc1609>
2020-12-17 12:40:53 +0100

    Carrito sin repetir

--- src/main/java/com/example/controladores/ProductoControlador.java
+++ src/main/java/com/example/controladores/ProductoControlador.java
@@ -1,7 +1,10 @@
 package com.example.controladores;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -21,23 +24,21 @@ import com.example.entidades.Producto;
 import com.example.entidades.ProductoCarrito;
 import com.example.servicios.ProductoServicio;
 
-
 @Controller
 @RequestMapping(value = "/Producto")
 public class ProductoControlador {
 
 	@Autowired
 	ProductoServicio productoService;
-	
-	/*@RequestMapping(method = RequestMethod.GET, value = "/lista")
-	public ModelAndView listarProducto() {
-		ModelAndView mav = new ModelAndView();
-		List<Producto> lProducto = productoService.listarProducto();
-		mav.addObject("Productos", lProducto);
-		mav.setViewName("Producto/Lista");
-		return mav;
-	}*/
-	
+
+	/*
+	 * @RequestMapping(method = RequestMethod.GET, value = "/lista") public
+	 * ModelAndView listarProducto() { ModelAndView mav = new ModelAndView();
+	 * List<Producto> lProducto = productoService.listarProducto();
+	 * mav.addObject("Productos", lProducto); mav.setViewName("Producto/Lista");
+	 * return mav; }
+	 */
+
 	@RequestMapping("/prueba")
 	public String hola(Model modelo) {
 		return "Producto/Prueba";
@@ -47,7 +48,7 @@ public class ProductoControlador {
 	public String showForm() {
 		return "Producto/crear";
 	}
-	
+
 	@RequestMapping(method = RequestMethod.GET, value = "/{id}")
 	public ModelAndView perfilProducto(@PathVariable("id") long idProducto, HttpServletRequest request) {
 
@@ -64,60 +65,83 @@ public class ProductoControlador {
 		mav.addObject("producto", producto);
 		mav.setViewName("producto/datos");
 		return mav;
-		
+
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST, value = "/crear")
-	public String crearProducto(
-			@RequestParam("nombre") String nombre, 
-			@RequestParam("marca") String marca,
-			@RequestParam("categoria") String categoria,
-			@RequestParam("descripcion") String descripcion,
-			@RequestParam("precio") int precio,
-			@RequestParam("descuento") int descuento,
-			HttpServletRequest request) {
-		
-		Producto producto = new Producto(marca,nombre,categoria,descripcion,precio,descuento);
+	public String crearProducto(@RequestParam("nombre") String nombre, @RequestParam("marca") String marca,
+			@RequestParam("categoria") String categoria, @RequestParam("descripcion") String descripcion,
+			@RequestParam("precio") int precio, @RequestParam("descuento") int descuento, HttpServletRequest request) {
+
+		Producto producto = new Producto(marca, nombre, categoria, descripcion, precio, descuento);
 		productoService.crearProducto(producto);
 		return "redirect:/";
 	}
 
-	@RequestMapping(method = RequestMethod.GET,value = "/borrar/{id}")
+	@RequestMapping(method = RequestMethod.GET, value = "/borrar/{id}")
 	public ModelAndView borrarProducto(@PathVariable("id") long idProducto, HttpServletRequest request) {
 		ModelAndView mav = new ModelAndView();
 		productoService.eliminarProducto(idProducto);
 		mav.setViewName("redirect:/");
 		return mav;
 	}
-	
+
 	@GetMapping("/carrito")
 	public String verCarrito(Model model, HttpSession session) {
-		
-		List<ProductoCarrito> carrito = (List<ProductoCarrito>)session.getAttribute("CARRITO");
-		if(carrito == null) {
-			carrito = new ArrayList<>();
+
+		Set<ProductoCarrito> carrito = (Set<ProductoCarrito>) session.getAttribute("CARRITO");
+		if (carrito == null) {
+			carrito = new HashSet<>();
 		}
-		
+
 		model.addAttribute("sessionCarrito", carrito);
 		return "Producto/carrito";
 	}
-	
+
 	@PostMapping("/añadirProducto")
-	public String añadirAlCarrito(
-			@RequestParam("id") Long id, 
-			@RequestParam("cantidadCarro") int cantidad,
+	public String añadirAlCarrito(@RequestParam("id") Long id, @RequestParam("cantidadCarro") int cantidad,
 			HttpServletRequest request) {
 		@SuppressWarnings("unchecked")
-		List<ProductoCarrito> carrito = (List<ProductoCarrito>) request.getSession().getAttribute("CARRITO");
+		
+		
+		Set<ProductoCarrito> carrito = (Set<ProductoCarrito>) request.getSession().getAttribute("CARRITO");
 		if (carrito == null) {
-			carrito = new ArrayList<>();
+			carrito = new HashSet<>();
+		}
+		
+		Boolean anadido =false;
+		
+		if (!carrito.isEmpty()) {
+			for (ProductoCarrito p :carrito) {
+				if (p.getIdProductoCarro()== id) {
+					p.setCantidadProductoCarro(p.getCantidadProductoCarro()+ cantidad);
+					anadido=true;
+					break;
+				}
+				
+				
+			}
+			
+		}
+		if (carrito.isEmpty() || !anadido) {
+			Producto producto = productoService.obtenerProducto(id);
+			ProductoCarrito productoCarrito = new ProductoCarrito(id, producto.getNombreProducto(), cantidad);
+			carrito.add(productoCarrito);
 		}
 
-		Producto producto = productoService.obtenerProducto(id);
-		ProductoCarrito productoCarrito = new ProductoCarrito(id,producto.getNombreProducto(),cantidad);
-		carrito.add(productoCarrito);
 		
-		request.getSession().setAttribute("CARRITO",carrito);
+//		Producto producto = productoService.obtenerProducto(id);
+//		ProductoCarrito productoCarrito = new ProductoCarrito(id, producto.getNombreProducto(), cantidad);
+//
+//		for (ProductoCarrito p : carrito) {
+//			if (p.equals(productoCarrito))
+//				productoCarrito.setCantidadProductoCarro(
+//						productoCarrito.getCantidadProductoCarro() + p.getCantidadProductoCarro());
+//		}
+//
+//		carrito.add(productoCarrito);
+
+		request.getSession().setAttribute("CARRITO", carrito);
 		return "redirect:/Producto/carrito";
 	}
 }
--- src/main/java/com/example/entidades/ProductoCarrito.java
+++ src/main/java/com/example/entidades/ProductoCarrito.java
@@ -1,4 +1,4 @@
-package com.example.entidades;
+ package com.example.entidades;
 
 
 public class ProductoCarrito {
@@ -43,5 +43,31 @@ public class ProductoCarrito {
 	public void setCantidadProductoCarro(int cantidadProductoCarro) {
 		this.cantidadProductoCarro = cantidadProductoCarro;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idProductoCarro == null) ? 0 : idProductoCarro.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ProductoCarrito other = (ProductoCarrito) obj;
+		if (idProductoCarro == null) {
+			if (other.idProductoCarro != null)
+				return false;
+		} else if (!idProductoCarro.equals(other.idProductoCarro))
+			return false;
+		return true;
+	}
+	
 	
 }
